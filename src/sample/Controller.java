04e3b2060e7dd1f8016c8237298aa0c0cb3c8dcb
Justas <justas.grekavicius@gmail.com>
2017-10-16 19:39:29 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+
+    @FXML
+    private Label myLabel;
+
+    @FXML
+    private TextField nameInput;
+
+    public void changeText(ActionEvent event){
+        String temp = myLabel.getText();
+        String name = nameInput.getText();
+        myLabel.setText(temp + " " + name);
+    }
+
+
+
+}
